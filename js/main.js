7ab9c7d858aa10a6eb6a2e8d86593c4498e70fcf
xuesh <534684120@qq.com>
2018-11-21 18:44:01 +0800

    cart data

--- js/main.js
+++ js/main.js
@@ -1,15 +1,45 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
+var app = new Vue({
   el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+  data: {
+    dataList: [],
+    totalMoney: 0
+  },
+  mounted: function(){
+    this.cartView();
+  },
+  methods: {
+    cartView: function(){
+      this.$http.get('./data/cartData.json').then(function(res){
+        console.log( res );
+        this.dataList = res.data.result.list;
+        this.totalMoney = res.data.result.totalMoney;
+      })
+    },
+    updateNumber: function(product, type){
+      if( type > 0 ){
+        product.productQuantity++;
+      }else if( type < 0){
+        product.productQuantity <= 1 ? 1: product.productQuantity--;
+      }
+      this.updateTotalMoney();
+    },
+    updateTotalMoney: function(){
+      let totalMoney = 0;
+      var dataList = this.dataList;
+      for(var i in dataList){
+        totalMoney += dataList[i].productPrice * dataList[i].productQuantity;
+      }
+      this.totalMoney = totalMoney;
+    },
+    selected: function(product){
+      if(typeof product.checked === 'undefined'){
+        this.$set(product, 'checked', true);
+      }else {
+        product.checked = !product.checked;
+      }
+      this.updateTotalMoney();
+    }
+
+  }
+  
 })
